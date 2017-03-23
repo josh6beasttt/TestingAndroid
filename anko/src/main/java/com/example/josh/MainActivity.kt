62f3a51e13c280a1c1dda611d2a98cc07b2f2bc7
Joshua Ramirez <josh@Joshuas-MacBook-Pro.local>
2017-03-23 18:06:57 -0400

    Extract layout to it's own class

--- anko/src/main/java/com/example/josh/MainActivity.kt
+++ anko/src/main/java/com/example/josh/MainActivity.kt
@@ -2,16 +2,22 @@ package com.example.josh
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import org.jetbrains.anko.textView
-import org.jetbrains.anko.verticalLayout
+import android.view.View
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        verticalLayout {
-            textView {
-                text = "Hello Josh"
+        MainActivityUi("World").setContentView(this)
+    }
+
+    class MainActivityUi(val name: String) : AnkoComponent<MainActivity> {
+        override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
+            verticalLayout {
+                textView {
+                    text = "Hello $name"
+                }
             }
         }
     }
